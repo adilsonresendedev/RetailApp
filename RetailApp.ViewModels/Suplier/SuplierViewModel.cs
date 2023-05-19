8cad65a2b0bf34120a4c5f0ea8ba70c5412a1f47
Adilson Resende <adilsonresendetp@gmail.com>
2023-05-18 21:29:48 -0300

    Added SuplierViewModel and it's dependencies.

---
+++ RetailApp.ViewModels/Suplier/SuplierViewModel.cs
@@ -0,0 +1,18 @@
+﻿using RetailApp.ViewModels.Address;
+using RetailApp.ViewModels.Phone;
+
+namespace RetailApp.ViewModels.Suplier
+{
+    public class SuplierViewModel
+    {
+        public int IdSuplier { get; set; }
+        public string CNPJ { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string ComercialName { get; set; } = default!;
+        public string IE { get; set; } = default!;
+        public string IEST { get; set; } = default!;
+        public int CRT { get; set; }
+        public List<AddresViewModel> addresViewModel { get; set; } = default!;
+        public List<PhoneViewModel> PhoneViewModel { get; set; } = default!;
+    }
+}
